de9b64d1dc048cb7c63b57f342279691957a0d4b
Deepanshu Vishwakarma <deepanshukarma7@gmail.com>
2026-08-13 09:42:25 +0530

    changes in api ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 ﻿import type { Student, UserProfile } from "../types";
 
-const API_BASE_URL = (import.meta as any).env.VITE_API_BASE_URL ?? "http://localhost:8000";
+const API_BASE_URL = (import.meta as any).env.VITE_API_URL ?? "http://localhost:8000";
 
 function getAuthToken() {
   return localStorage.getItem("token");
